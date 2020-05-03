470530fdcd666d8f7137b9d0b52c45094a8917f4
Mario Gonzalez <mariogl00@gmail.com>
2020-05-02 19:46:01 -0500

    Adding angular static page

--- src/main/java/com/example/todo/entity/Todo.java
+++ src/main/java/com/example/todo/entity/Todo.java
@@ -13,6 +13,7 @@ public class Todo {
 
     private String title;
     private String description;
+    private boolean completed;
 
     public Todo() {
     }
@@ -20,6 +21,7 @@ public class Todo {
     public Todo(String title, String description) {
         this.title = title;
         this.description = description;
+        this.completed = false;
     }
 
     public Long getId() {
@@ -44,6 +46,14 @@ public class Todo {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 
     @Override
